95ca01fafb26c98f83d32357b08854128f62e6aa
DESKTOP-P7TMH9A\PC-14 <thdnjs0221@naver.com>
2023-06-05 18:31:36 +0900

    6/5 수업

--- 230526_SQL.sql
+++ 230526_SQL.sql
@@ -2229,6 +2229,7 @@ END LOOP;
 END;
 
 --430
+
 DECLARE
 TYPE STARCRAFT IS VARRAY(20) OF VARCHAR2(10);--TYPE선언
 V_STAR STARCRAFT;
@@ -2241,3 +2242,159 @@ FOR I IN V_STAR.FIRST..V_STAR.LAST LOOP
 DBMS_OUTPUT.PUT_LINE(I||'번째 종족:'||V_STAR(I));
 END LOOP;
 END;
+
+--SET SERVEROUTPUT ON 
+
+--432
+DECLARE
+TYPE lprod_nm_table IS TABLE OF VARCHAR2(40)
+    INDEX BY PLS_INTEGER;
+    
+t_lpord_nm lprod_nm_table;
+BEGIN
+FOR l_list IN (SELECT LPROD_ID, LPROD_NM FROM LPROD) LOOP
+T_LPROD_NM(L_LIST.LPROD_ID ):=L_LIST.LPROD_NM;
+DBMS_OUTPUT.PUT_LINE(L_LIST.LPROD_ID||'='||L_LIST.LPROD_NM);
+END LOOP;
+
+DBMS_OUTPUT.PUT_LINE('갯수='||T_LPROD_NM.COUNT);
+FOR I IN T_LPROD_NM.FIRST..T_LPROD_NM.LAST LOOP
+IF T_LPROD_NM.EXISTS(I) THEN
+DBMS_OUTPUT.PUT_LINE(I||''||T_LPROD_NM(I));
+END IF;
+END LOOP;
+END;
+
+DECLARE
+  TYPE lprod_nm_table IS TABLE OF VARCHAR2(40)
+      INDEX BY PLS_INTEGER;
+  t_lprod_nm lprod_nm_table;
+  
+BEGIN  
+  FOR  l_list IN (SELECT lprod_id, lprod_nm FROM lprod) LOOP
+      t_lprod_nm(l_list.lprod_id) := l_list.lprod_nm;
+      DBMS_OUTPUT.PUT_LINE (l_list.lprod_id || ' = ' || l_list.lprod_nm  );
+  END LOOP;
+
+  DBMS_OUTPUT.PUT_LINE ('갯수 = ' || t_lprod_nm.COUNT); 
+  FOR i IN t_lprod_nm.FIRST..t_lprod_nm.LAST LOOP
+	IF t_lprod_nm.EXISTS(i) THEN
+  	   DBMS_OUTPUT.PUT_LINE ( i || ' ' || t_lprod_nm(i));
+	END IF;
+   END LOOP;
+END;
+
+--436
+DECLARE
+v_name varchar2(20);
+BEGIN
+SELECT lprod_nm INTO v_name FROM lprod WHERE lprod_gu='P201';
+DBMS_OUTPUT.PUT_LINE('분류명=' ||v_name);
+
+EXCEPTION 
+WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('해당 정보가 없습니다');
+ WHEN TOO_MANY_ROWS THEN
+    DBMS_OUTPUT.PUT_LINE('한개 이상의 값이 나왔습니다');
+WHEN  OTHERS  THEN
+    DBMS_OUTPUT.PUT_LINE('기타 에러:' ||SQLERRM );
+END;
+
+--437
+DECLARE
+exp_reference EXCEPTION;   --예외선언
+PRAGMA EXCEPTION_INIT(exp_reference,-2292);
+BEGIN
+DELETE FROM lprod WHERE lprod_gu='P101';
+DBMS_OUTPUT.PUT_LINE('분류삭제');
+EXCEPTION --해당예외처리
+WHEN exp_reference THEN --예외가 발생했을때 처리할 코드 정의
+DBMS_OUTPUT.PUT_LINE('삭제불가:'||SQLERRM); --SQLERRM: 현재 예외에 대한 오류메세지를 반환하는 함수 
+WHEN OTHERS THEN --이외의 예외가 발생했을 때 처리할 코드 정의
+DBMS_OUTPUT.PUT_LINE(SQLCODE||''||SQLERRM);
+END;
+/
+--438
+ACCEPT p_lgu PROMPT '등록하려는 분류코드 입력 :'
+DECLARE 
+   exp_lprod_gu EXCEPTION;
+   v_lgu VARCHAR2(10) := UPPER('&p_lgu');
+BEGIN
+   IF v_lgu IN ('P101','P102','P201','P202') THEN
+        RAISE exp_lprod_gu;
+   END IF;
+   DBMS_OUTPUT.PUT_LINE (v_lgu || '는 등록 가능');
+EXCEPTION 
+     WHEN exp_lprod_gu THEN
+           DBMS_OUTPUT.PUT_LINE ( v_lgu ||  '는 이미 등록된 코드 입니다.');
+END;
+/
+--443 2020년도 및 상품별 총 입고수량을 출력하는 커서
+DECLARE
+v_prod VARCHAR2(30);
+v_qty NUMBER(10,0);
+CURSOR UpRemain_cur IS  -- UpRemain_cur 선언해라 
+SELECT buy_prod, SUM(buy_qty) FROM buyprod-- 읽어야할 쿼리
+WHERE EXTRACT (YEAR FROM buy_date)=2020
+GROUP BY buy_prod ORDER BY buy_prod ASC;
+
+BEGIN
+OPEN UpRemain_cur;
+FETCH UpRemain_cur INTO v_prod, v_qty;
+WHILE (UpRemain_cur%FOUND)  LOOP
+DBMS_OUTPUT.PUT_LINE(UpRemain_cur%ROWCOUNT ||'번째 상품='
+                                           ||v_prod||'입고수량='
+                                           ||v_qty|| '입니다');
+FETCH UpRemain_cur INTO v_prod, v_qty;
+END LOOP;
+CLOSE UpRemain_cur;
+END;
+/
+
+DECLARE
+v_name VARCHAR2(30);
+v_mileage NUMBER(10);
+
+CURSOR member_cur (v_job VARCHAR2) IS
+SELECT mem_name, mem_mileage FROM member
+WHERE mem_job = v_job
+ORDER BY mem_name ASC;
+BEGIN
+OPEN member_cur('공무원');
+LOOP
+FETCH member_cur INTO v_name, v_mileage;
+EXIT WHEN member_cur%NOTFOUND;
+DBMS_OUTPUT.PUT_LINE(member_cur%ROWCOUNT||'번째'
+                                        ||v_name||','||v_mileage);
+END LOOP;
+CLOSE member_cur;
+END;
+/
+--446
+DECLARE
+v_name VARCHAR2(30);
+v_mileage NUMBER(10);
+CURSOR member-cur IS
+SELECT mem_name, mem_mileage
+FROM member
+WHERE mem_job='&p_job'
+ORDER BY mem_name ASC;
+BEGIN
+FOR mem_rec IN mem_cur LOOP
+DBMS_OUTPUT.PUT_LINE(member_cur%ROWCOUNT ||'번째'
+                    ||mem_rec.mem_name||','||mem_rec.mem_mileage);
+                    END LOOP;
+                    END;
+                    /
+--447
+BEGIN 
+FOR mem_rec IN (SELECT mem_name,mem_mileage
+FROM member ORDER BY mem_name ASC)LOOP
+DBMS_OUTPUT.PUT_LINE(mem_rec.mem_id ||','
+                        || mem_rec.mem_name ||','
+                        || mem_rec.emem_mileage);
+                        END LOOP;
+                        END;
+                        /
+                        
+
